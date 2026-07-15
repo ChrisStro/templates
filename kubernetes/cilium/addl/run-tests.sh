70a2a1dd9ec7368c8150ec0658118eab9fb49851
ChrisStro <christopher.strobel@outlook.de>
2026-07-15 17:51:36 +0200

    update cilium

--- kubernetes/cilium/addl/run-tests.sh
+++ kubernetes/cilium/addl/run-tests.sh
@@ -1,2 +1,2 @@
 #! /usr/local/bin/env bash
-kubectl apply -n cilium -f https://raw.githubusercontent.com/cilium/connectivity-check/main/connectivity-check.yaml
+kubectl apply -n cilium-system -f https://raw.githubusercontent.com/cilium/connectivity-check/main/connectivity-check.yaml
